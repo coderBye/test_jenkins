4b224b1bc4f2045c36cf0b569e32cd18ff6ae14b
周勇 <coderbye@163.com>
2023-03-31 12:43:47 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <h2>coderbye's app</h2>
+    <h2>coderbye's app 2023 3 31</h2>
     <router-link to="/">home</router-link>
     <router-link to="/about">about</router-link>
     <router-view></router-view>
